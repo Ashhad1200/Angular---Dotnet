a8685fa52afa38ffdcb312d55d25f4fa1a759716
Syed Ashhad <syedashad17@gmail.com>
2025-01-31 13:55:23 +0500

    Add CORS support, user data fetching, and update app configuration

--- DatingApp/clint/src/app/app.component.ts
+++ DatingApp/clint/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +7,25 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'Dating';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5000/api/user').subscribe({
+      next: (data) => {
+        console.log(data);
+        this.users = data;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
+  }
 }
